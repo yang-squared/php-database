475715f9da758f89b5edc35d8144d9f8f2f07d7c
seungjae-yang <61580903+yang-squared@users.noreply.github.com>
2022-03-22 17:04:02 +0900

    php&database 연동

---
+++ php&DATAbase/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>WEB</title>
+  </head>
+  <body>
+    <h1>WEB</h1>
+    <ol>
+      <li>HTML</li>
+    </ol>
+    <h2>Welcome</h2>
+    데이터베이스 php 연동 연습페이지
+  </body>
+</html>
